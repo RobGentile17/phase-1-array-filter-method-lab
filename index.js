6ea474671d985d76294fab8b76810fb527f4fc09
Rob <robert.gentile@powerhrg.com>
2021-05-26 14:32:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const findMatching = (drivers, string) => {
+    return drivers.filter(function hello (returnDrivers) {
+       return  returnDrivers === string || returnDrivers === string.toLowerCase(drivers)
+    })
+}
+
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter(function yes(driver){
+       return driver.startsWith(string)
+    })
+}
+
+const matchName = (drivers, name) => {
+return drivers.filter(function (driverObject) {
+    return driverObject.name === name
+} )
+}
